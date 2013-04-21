f9ad0e801c65196d4df1209252767ec5c946f5e8
Jordan Rogers-Smith <jordan.rogers-smith@betapond.com>
2013-04-21 10:39:41 +0100

    acces control hearder

--- app/controllers/soil_samples_controller.rb
+++ app/controllers/soil_samples_controller.rb
@@ -1,6 +1,15 @@
 class SoilSamplesController < ApplicationController
   # GET /soil_samples
   # GET /soil_samples.json
+  before_filter :allow_all
+  
+  def allow_all
+    headers['Access-Control-Allow-Origin'] = '*'
+    # headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, HEAD, PUT, DELETE'
+    # headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
+    # headers['Access-Control-Max-Age'] = '86400'
+  end
+  
   def index
    
       @soil_samples =  SoilSample.unscoped
