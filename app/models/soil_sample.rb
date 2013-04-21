525fec89f1351dee1f9664da00b92dc4aba7be87
Jordan Rogers-Smith <jordan.rogers-smith@betapond.com>
2013-04-21 09:51:06 +0100

    changed model accessor

--- app/models/soil_sample.rb
+++ app/models/soil_sample.rb
@@ -1,5 +1,5 @@
 class SoilSample < ActiveRecord::Base
-  attr_accessible :lat, :long, :moisture, :pH, :temperature, :time
+  attr_accessible :lat, :long, :moisture, :pH, :temperature, :time, :device_id, :comments
   
   validates :lat, :numericality => true    
   validates :long, :numericality => true
