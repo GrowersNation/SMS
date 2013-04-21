6150694b17e6f17f480148f680a3a09cfcceb4d7
Jordan Rogers-Smith <jordan.rogers-smith@betapond.com>
2013-04-21 10:58:09 +0100

    removed validations

--- app/controllers/soil_samples_controller.rb
+++ app/controllers/soil_samples_controller.rb
@@ -5,9 +5,7 @@ class SoilSamplesController < ApplicationController
   
   def allow_all
     headers['Access-Control-Allow-Origin'] = '*'
-    # headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, HEAD, PUT, DELETE'
-    # headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
-    # headers['Access-Control-Max-Age'] = '86400'
+    
   end
   
   def index
--- app/models/soil_sample.rb
+++ app/models/soil_sample.rb
@@ -1,11 +1,11 @@
 class SoilSample < ActiveRecord::Base
   attr_accessible :lat, :long, :moisture, :pH, :temperature, :time, :device_id, :comments, :device
   
-  # refractor
-  validates :lat, :numericality => true, :allow_blank => true   
-  validates :long, :numericality => true, :allow_blank => true  
-  validates :moisture, :numericality => true, :allow_blank => true  
-  validates :pH, :numericality => true, :allow_blank => true  
-  validates :temperature, :numericality => true, :allow_blank => true  
-  validates :time, :numericality => true, :allow_blank => true  
+  # # refractor
+  # validates :lat, :numericality => true, :allow_blank => true   
+  # validates :long, :numericality => true, :allow_blank => true  
+  # validates :moisture, :numericality => true, :allow_blank => true  
+  # validates :pH, :numericality => true, :allow_blank => true  
+  # validates :temperature, :numericality => true, :allow_blank => true  
+  # validates :time, :numericality => true, :allow_blank => true  
 end
